7e60c88e5d6924c2b0768dba9835f1c722c74cd4
Zsolt Szabo <zsolt.szabo@liferay.com>
2016-08-17 09:58:14 +0200

    Moving slider component from main.js to the related article template

--- themes/grow-theme/src/js/main.js
+++ themes/grow-theme/src/js/main.js
@@ -8,27 +8,3 @@ AUI().ready(
 		}
 	}
 );
-
-/*
- * Revolution custom js file
- * Author: Design_mylife
- * 
- */
-
-//full width slider
-
-
-jQuery(document).ready(function () {
-
-    revapi = jQuery('.tp-banner').revolution(
-            {
-                delay: 6000,
-                startwidth: 1170,
-                startheight: 600,
-                hideThumbs: 10,
-                fullWidth: "on",
-                forceFullWidth: "on",
-                navigationStyle: "preview4"
-            });
-
-});
