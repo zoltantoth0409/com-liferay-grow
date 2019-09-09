74004d76e69e571f4fa62183c32df225ef4a6c0e
Roland Pákai <roland.pakai@liferay.com>
2019-09-09 13:27:21 +0200

    GROW-898 Init portlet

---
+++ modules/highlighted-recommendations-portlet/src/main/java/highlightedrecommendationportlet/constants/HighlightedRecommendationPortletKeys.java
@@ -0,0 +1,10 @@
+package highlightedrecommendationportlet.constants;
+
+/**
+ * @author rolandpakai
+ */
+public class HighlightedRecommendationPortletKeys {
+
+	public static final String HighlightedRecommendation = "highlightedrecommendation";
+
+}
