bef9f1e966e5baf07adbafba052aa74df0bc6a24
hudakl <laszlo.hudak@liferay.com>
2020-02-07 16:06:50 +0100

    GROW-932 Create service component

---
+++ modules/wiki-webcontent-migration/src/main/java/com/liferay/grow/wiki/migration/WikiMigration.java
@@ -0,0 +1,21 @@
+package com.liferay.grow.wiki.migration;
+
+import com.liferay.grow.wiki.migration.WikiMigration;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Liferay
+ */
+@Component(
+	immediate = true,
+	property = {
+		// TODO enter required service properties
+	},
+	service = WikiMigration.class
+)
+public class WikiMigration {
+
+	// TODO enter required service methods
+
+}
