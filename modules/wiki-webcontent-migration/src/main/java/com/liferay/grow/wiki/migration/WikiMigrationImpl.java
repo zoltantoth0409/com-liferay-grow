107be51c02761998fc3180260ec8ca9409ca452a
hudakl <laszlo.hudak@liferay.com>
2020-02-10 18:07:38 +0100

    OWXP-932 Get content of the wikipage in web content's xml formar

--- modules/wiki-webcontent-migration/src/main/java/com/liferay/grow/wiki/migration/WikiMigrationImpl.java
+++ modules/wiki-webcontent-migration/src/main/java/com/liferay/grow/wiki/migration/WikiMigrationImpl.java
@@ -8,9 +8,14 @@ import com.liferay.dynamic.data.mapping.model.DDMStructure;
 import com.liferay.dynamic.data.mapping.model.DDMTemplate;
 import com.liferay.dynamic.data.mapping.service.DDMStructureLocalServiceUtil;
 import com.liferay.journal.model.JournalArticle;
+import com.liferay.petra.xml.XMLUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.xml.Document;
+import com.liferay.portal.kernel.xml.Element;
+import com.liferay.portal.kernel.xml.SAXReaderUtil;
 import com.liferay.wiki.model.WikiPage;
+import com.liferay.wiki.model.WikiPageDisplay;
 import com.liferay.wiki.service.WikiPageLocalServiceUtil;
 
 import java.util.Arrays;
@@ -53,6 +58,56 @@ public class WikiMigrationImpl implements WikiMigration {
 
 		pages = WikiPageLocalServiceUtil.getPages("creole");
 		System.out.println("n="+pages.size());
+	}
+
+	public String getContentXml(WikiPage page) {
+		try {
+			String format = page.getFormat();
+
+			if (!format.equalsIgnoreCase("markdown")) {
+				format = "html";
+			}
+
+			WikiPageDisplay display = WikiPageLocalServiceUtil.getPageDisplay(
+				page, null, null, "");
+
+			String content = display.getFormattedContent();
+
+			System.out.println("FormattedContent=\n>>>>>>>>>>>>\n"+content+"\n<<<<<<<<<<<<<<<<");
+
+			Document document = SAXReaderUtil.createDocument();
+
+			Element rootElement = document.addElement("root");
+
+			rootElement.addAttribute("available-locales", "en_US");
+			rootElement.addAttribute("default-locale", "en_US");
+
+			addElement(rootElement, "format", "list", "keyword", "bgea", "en_US", format);
+			addElement(rootElement, "content", "text_area", "text", "clfy", "en_US", content);
+			addElement(rootElement, "ParentArticle", "ddm-journal-article", "keyword", "dswq", "en_US", "");
+
+			return XMLUtil.formatXML(document.asXML());
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+	private void addElement(Element rootElement, String name, String type, 
+		String indexType, String instanceId, String languageId, 
+		String content) {
+		
+		Element dynamicElementElement = rootElement.addElement("dynamic-element");
+		dynamicElementElement.addAttribute("name", name);
+		dynamicElementElement.addAttribute("type", type);
+		dynamicElementElement.addAttribute("index-type", indexType);
+		dynamicElementElement.addAttribute("instance-id", instanceId);
+
+		Element dynamicContentElement = dynamicElementElement.addElement("dynamic-content");
+		dynamicContentElement.addAttribute("language-id", languageId);
+		dynamicContentElement.addCDATA(content);		
 	}
 
 	private void handleChildPages(
