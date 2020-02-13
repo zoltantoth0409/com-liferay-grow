30ac6a2d8c858bb90fa31d5f81409920f4a36fca
Norbert Kocsis <norbert.kocsis@liferay.com>
2020-02-13 12:46:40 +0100

    GROW-1002 Wait for PORTAL_INITIALIZED

--- grow-favourites/grow-favourites-rest/src/main/java/com/grow/favourites/rest/resource/FavouritesResource.java
+++ grow-favourites/grow-favourites-rest/src/main/java/com/grow/favourites/rest/resource/FavouritesResource.java
@@ -39,6 +39,7 @@ import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.model.UserConstants;
+import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.service.UserLocalServiceUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
@@ -49,6 +50,7 @@ import com.liferay.wiki.service.WikiPageLocalServiceUtil;
 import com.liferay.portal.kernel.util.Validator;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -431,5 +433,8 @@ public class FavouritesResource {
 
 		return responseBuilder.build();
 	}
+
+	@Reference(target = ModuleServiceLifecycle.PORTAL_INITIALIZED)
+	private ModuleServiceLifecycle _portalInitialized;
 
 }
