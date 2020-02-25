a400bd04f2bcfaf78d3299fdfb788a547f36b5dd
hudakl <laszlo.hudak@liferay.com>
2020-02-25 09:52:45 +0100

    GROW-932 Fix tree iteration

--- modules/wiki-webcontent-migration/src/main/java/com/liferay/grow/wiki/migration/WikiMigrationImpl.java
+++ modules/wiki-webcontent-migration/src/main/java/com/liferay/grow/wiki/migration/WikiMigrationImpl.java
@@ -44,7 +44,7 @@ public class WikiMigrationImpl implements WikiMigration {
 		init();
 
 		for (WikiPage page : pages) {
-			if (Validator.isNull(page.getParentTitle())) {
+			if (Validator.isNotNull(page.getParentTitle())) {
 				continue;
 			}
 
