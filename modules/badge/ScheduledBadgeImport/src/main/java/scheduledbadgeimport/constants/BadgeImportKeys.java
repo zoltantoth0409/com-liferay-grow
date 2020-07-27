80ce79f84e3063cf7e607ef0b18c6865b6b0c44b
IstvanD <istvan.dezsi@liferay.com>
2020-07-27 11:01:58 +0200

    GROW-1148 Adding BadgeImportKeys

---
+++ modules/badge/ScheduledBadgeImport/src/main/java/scheduledbadgeimport/constants/BadgeImportKeys.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package scheduledbadgeimport.constants;
+
+/**
+ * @author István András Dézsi
+ */
+public class BadgeImportKeys {
+
+	public static final long COMPANY_ID = 20101;
+
+	public static final boolean DRYRUN = true;
+
+	public static final String FIRST_ARTICLE_BADGE_VIEW_ID =
+		"5bca7eae-1b9b-4b87-8d11-58a2ed00450a";
+
+	public static final String FIRST_CLOSED_TICKET_BADGE_VIEW_ID =
+		"f8d45d38-2799-43ec-928b-abaf0cf42a34";
+
+	public static final long FROM_USER_ID = 20130;
+
+	public static final String FROM_USER_NAME = "Test Test";
+
+	public static final String INITIAL_LOYALTY_BADGE_VIEW_ID =
+		"70227c7d-f4e3-4bad-8bbd-f09d69071933";
+
+	public static final String LOYALTY_BADGE_VIEW_ID =
+		"85ac4a1d-efe0-4504-bf6e-ebc706fe3440";
+
+	public static final String SUPPORT_SITE_ID =
+		"2ec06f59-1e6d-42c1-9f68-f6c525e733c0";
+
+	public static final String TABLEAU_AUTH_TOKEN =
+		"F0fWSXq9SbCkweZ5U1esLA|gIKcvHaQG5x79lRmN0mB0MNsmwXjAMpw";
+
+}
